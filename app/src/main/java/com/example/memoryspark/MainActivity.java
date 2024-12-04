57960887ca004f886f5f4c7c6c672d94912c5d53
Keiinz <hasnawidhiakusuma@gmail.com>
2024-12-04 22:18:09 +0700

    Complete add deck & card UseCase

---
+++ app/src/main/java/com/example/memoryspark/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.memoryspark;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_container);
+
+        if (savedInstanceState == null) {
+            loadFragment(new DeckFragment());
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
+    }
+}
